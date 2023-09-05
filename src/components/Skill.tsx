f81b6d28df5a7c4b479aa975295dad6da7eb4a57
littledev13 <irvandn13@gmail.com>
2023-09-05 22:45:57 +0700

    update 2

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import IconCss from './Icon/IconCss'
+import IconExpress from './Icon/IconExpress'
+import IconLaravel from './Icon/IconLaravel'
+import IconMySql from './Icon/IconMySql'
+import IconNextjs from './Icon/IconNextJS'
+import IconNodeJs from './Icon/IconNodeJs'
+import IconTailwind from './Icon/IconTailwind'
+import IconReact from './Icon/IconReact'
+import IcoonLaravel from './Icon/IcoonLaravel'
 
 function Skill() {
   return (
     <div id='skill'>
-        <div id="title-section">SKILL</div>
-        <div id="skill-section">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ex inventore distinctio magni sequi architecto atque, incidunt vel quo alias id dignissimos in, harum facilis aspernatur quia tenetur debitis nulla doloremque libero placeat numquam molestiae praesentium sapiente! Repellat vel aliquid consequatur sunt, maiores inventore nobis aspernatur. Corporis tempore esse corrupti.
+        <div id="tittle-section">SKILL</div>
+        <div id="skill-section" className='container grid-cols-4 grid-rows-4 bg-green-500'>
+            <IconCss/>
+            <IconExpress/>
+            <IconLaravel/>
+            <IconMySql/>
+            <IconNextjs/>
+            <IconNodeJs/>
+            <IconTailwind/>
+            <IconReact/>
+            <IcoonLaravel/>
         </div>
     </div>
   )
